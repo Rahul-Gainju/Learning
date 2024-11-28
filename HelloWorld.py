0c849523841b9a1e412bfba56c98313d754a3603
Rahul Gainju <rahulgainju03@gmail.com>
2024-11-28 19:57:03 +0545

    sum

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,8 @@
 print("Hello World")
+
+# sum
+
+first = float(input("Enter the first number: "))
+second = float(input("Enter the second number: "))
+sum = first + second
+print("The sum of a and b is: " + str(sum))
